b3ed539ad7851950df291299683d4ef8bab2eb01
HyunAm <tlfgjawlq@naver.com>
2020-12-22 16:34:32 +0900

    0.4 GET,POST 실습하기

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import HelloWorld
 
 
 def hello(request):
-    return render(request, 'accountapp/hello.html')
+    if request.method == "POST":
+        temp = request.POST.get('hello_world_input')
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+        return render(request, 'accountapp/hello.html', context={'hello_world_output': new_hello_world})
+    else:
+        return render(request, 'accountapp/hello.html', context={'text': 'get mothod!'})
